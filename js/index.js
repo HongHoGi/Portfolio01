860355419f5ad10ce23e7da4542d45aecbfc24bc
HongHoGi <85734676+HongHoGi@users.noreply.github.com>
2021-10-27 16:30:39 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// 슬라이드 부분
+$('.slideInner').slick({
+    autoplay : true,         
+    autoplaySpeed : 3000,  
+    arrows : false, 
+    dots : true,            
+    pauseOnHover: false,
+})
+
+
